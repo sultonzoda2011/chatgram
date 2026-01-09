de37ad51619c418ca882811d40f25e797b26185a
Abdulloh <sultonzoda2011@gmail.com>
2026-01-09 12:45:06 +0500

    feat: добавлены интерфейсы ILogin и IRegister для форм аутентификации

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,2 +1,10 @@
-export interface ILogin {}
-export interface IRegister {}
+export interface ILogin {
+  username: string
+  password: string
+}
+export interface IRegister {
+  username: string
+  fullname: string
+  email: string
+  password: string
+}
