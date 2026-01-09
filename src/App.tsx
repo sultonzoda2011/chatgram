718911b53ac7d742baa564f4b06b5a6341497e81
Abdulloh <sultonzoda2011@gmail.com>
2026-01-09 12:41:42 +0500

    feat: настроена маршрутизация с React Router, ProtectedRoute и Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,38 @@
+// App.tsx
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './layout/layout'
+import LoginPage from '@/pages/(auth)/login'
+import RegisterPage from '@/pages/(auth)/register'
+import { ProtectedRoute } from '@/layout/protectedRoute'
+import HomePage from '@/pages/main/home'
+
 const App = () => {
-  return <div>App</div>
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+      ],
+    },
+    {
+      path: '/login',
+      element: <LoginPage />,
+    },
+    {
+      path: '/register',
+      element: <RegisterPage />,
+    },
+  ])
+
+  return <RouterProvider router={router} />
 }
 
 export default App
