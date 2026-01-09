9e322cd3824d87b6d9ff0b9189cea91fcf136769
Abdulloh <sultonzoda2011@gmail.com>
2026-01-09 12:42:11 +0500

    feat: добавлены API-функции loginApi и registerApi для аутентификации

---
+++ src/api/authApi.ts
@@ -0,0 +1,12 @@
+import { api } from '@/lib/utils/axiosConfig'
+import type { ILogin, IRegister } from '@/types/auth'
+
+export const loginApi = async (data: ILogin) => {
+  const response = await api.post('/auth/login', data)
+  return response.data
+}
+
+export const registerApi = async (data: IRegister) => {
+  const response = await api.post('/auth/register', data)
+  return response.data
+}
