52e90bd8e59a1aacedc0bcf7453a174d812b8768
Abdulloh <sultonzoda2011@gmail.com>
2026-01-12 07:35:31 +0500

    feat: добавлен I18nextProvider в Providers для поддержки мультиязычности

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -1,12 +1,18 @@
 import { type ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
+import { I18nextProvider } from 'react-i18next'
+import i18n from '@/i18n/i18n'
+
 const queryClient = new QueryClient()
+
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster closeButton richColors />
-      {children}
+      <I18nextProvider i18n={i18n}>
+        <Toaster closeButton richColors />
+        {children}
+      </I18nextProvider>
     </QueryClientProvider>
   )
 }
