e0d237d890a5983398e4bb24d43818064da2cb86
Abdulloh <sultonzoda2011@gmail.com>
2026-01-26 06:55:51 +0500

    feat(ui): add ThemeProvider to global Providers component

--- src/lib/providers/providers.tsx
+++ src/lib/providers/providers.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 import { I18nextProvider } from 'react-i18next'
 import i18n from '@/i18n/i18n'
+import { ThemeProvider } from '@/lib/providers/theme-provider'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +12,9 @@ const Providers = ({ children }: { children: ReactNode }) => {
     <QueryClientProvider client={queryClient}>
       <I18nextProvider i18n={i18n}>
         <Toaster closeButton richColors />
-        {children}
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          {children}
+        </ThemeProvider>
       </I18nextProvider>
     </QueryClientProvider>
   )
