95de56a5693d04ff1cdb7db1e20ca22897062ebe
Abdulloh <sultonzoda2011@gmail.com>
2026-01-27 06:38:48 +0500

    feat: enhance Layout component with motion effects for smoother transitions

--- src/components/ui/layout/layout.tsx
+++ src/components/ui/layout/layout.tsx
@@ -1,7 +1,9 @@
 import BottomNav from '@/components/ui/bottomnav/bottomnav'
+import Header from '@/components/ui/layout/header'
 import { getToken, removeToken } from '@/lib/utils/cookie'
 import { isTokenExpired } from '@/lib/utils/jwt'
 import { Navigate, Outlet } from 'react-router-dom'
+import { motion } from 'motion/react'
 
 const Layout = () => {
   const token = getToken()
@@ -12,10 +14,18 @@ const Layout = () => {
   }
 
   return (
-    <div className="flex flex-col min-h-screen pb-20">
-      <Outlet />
+    <motion.div
+      className="flex flex-col min-h-screen bg-background"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Header />
+      <main className="flex-1 pb-20">
+        <Outlet />
+      </main>
       <BottomNav />
-    </div>
+    </motion.div>
   )
 }
 
