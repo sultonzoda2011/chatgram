bea533bae22944d2bd3305183a944208f597b558
Abdulloh <sultonzoda2011@gmail.com>
2026-01-27 06:37:45 +0500

    feat: integrate motion for animated label component

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
+import { motion } from "motion/react"
 
 import { cn } from "@/lib/utils/shadcn"
 
@@ -9,13 +10,20 @@ function Label({
 }: React.ComponentProps<typeof LabelPrimitive.Root>) {
   return (
     <LabelPrimitive.Root
+      asChild
       data-slot="label"
-      className={cn(
-        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className
-      )}
-      {...props}
-    />
+    >
+      <motion.label
+        className={cn(
+          "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+          className
+        )}
+        initial={{ opacity: 0, y: -5 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.2 }}
+        {...props}
+      />
+    </LabelPrimitive.Root>
   )
 }
 
