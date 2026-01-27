a27e801541a523eea6068c5453e7a11c6a5a8e0d
Abdulloh <sultonzoda2011@gmail.com>
2026-01-27 06:37:37 +0500

    feat: add UI component exports for loading, error, button, input, and label

---
+++ src/components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Loading, LoadingDots, LoadingSpinner } from './loading'
+export type { LoadingProps } from './loading'
+
+export { ErrorDisplay, ErrorBoundaryFallback, ErrorInline } from './error'
+export type { ErrorProps } from './error'
+
+export { Button } from './button/button'
+export { Input } from './input/input'
+export { Label } from './label'
