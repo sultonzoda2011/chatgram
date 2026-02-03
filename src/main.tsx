84a8fe8e3aff4417b8d87c8496d9d8a6ad00a0a6
Abdulloh <sultonzoda2011@gmail.com>
2026-02-03 07:31:10 +0500

    feat:manifest and server worker

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import Providers from './lib/providers/providers.tsx'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({
+  immediate: true,
+})
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Providers>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,42 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import path from 'path'
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: [
+        'icons/icon-192.png',
+        'icons/icon-512.png'
+      ],
+      manifest: {
+        name: 'ChatGram',
+        short_name: 'ChatGram',
+        description: 'A modern responsive chat application.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
